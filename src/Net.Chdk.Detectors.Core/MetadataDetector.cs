2f7914fb6a03d9b1a9b44441e30c70d8fa686d1d
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 17:26:41 +0300

    Add solution

---
+++ src/Net.Chdk.Detectors.Core/MetadataDetector.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Net.Chdk.Detectors
+{
+    public abstract class MetadataDetector<TDetector, TValue>
+        where TDetector : MetadataDetector<TDetector, TValue>
+        where TValue : class
+    {
+        protected ILogger Logger { get; }
+
+        protected MetadataDetector(ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger<TDetector>();
+        }
+
+        protected abstract string FileName { get; }
+
+        protected TValue GetValue(string driveLetter)
+        {
+            var metadataPath = Path.Combine(driveLetter, "METADATA");
+            var filePath = Path.Combine(metadataPath, FileName);
+            if (!File.Exists(filePath))
+                return null;
+
+            Logger.LogInformation("Reading {0}", filePath);
+
+            using (var stream = File.OpenRead(filePath))
+            {
+                return JsonObject.Deserialize<TValue>(stream);
+            }
+        }
+    }
+}
